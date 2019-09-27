b28f489b024a0088bd5d90c4ffeee7a36930d31e
KingdomCross <alexchheng1998@yahoo.com>
2019-09-27 16:01:33 -0700

    Initial commit

---
+++ Person.cpp
@@ -0,0 +1,10 @@
+//
+// Created by alexc on 9/26/2019.
+//
+
+#include "Person.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
---
+++ Person.h
@@ -0,0 +1,21 @@
+//
+// Created by alexc on 9/26/2019.
+//
+
+#ifndef PERSON_PERSON_H
+#define PERSON_PERSON_H
+
+#include <string>
+
+using namespace std;
+class Person {
+    string name;
+    double salary;
+public:
+    void setPerson();
+    string getName();
+    float getSalary();
+};
+
+
+#endif //PERSON_PERSON_H
