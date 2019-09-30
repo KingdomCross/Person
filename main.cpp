63a284659c64fa5716b1202a59236ea6a45f3ff5
KingdomCross <alexchheng1998@yahoo.com>
2019-09-30 05:33:28 -0700

    Input and function

--- main.cpp
+++ main.cpp
@@ -3,7 +3,30 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    person my_person("Alex", 25);
-    my_person.setPerson();
+    //person my_person("Alex", 25);
+    //my_person.setPerson();
+    int arr[5];
+    int *p = arr;
+    cout << "Enter array elements \n";
+    for(int i = 0; i < 5; i++){
+        cin >> *(p + i);
+    }
+
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < (5 - i - 1); j++){
+            if (*(p + j) > *(p +(j + 1))){
+                int t = *(p + j);
+                *(p + j) = *(p +(j + 1));
+                *(p +(j + 1)) = t;
+            }
+        }
+    }
+    cout << "\nArray after sorting are \n";
+    for(int i = 0; i < 5; i++){
+        cout << *(p + i) << " ";
+    }
+
+    int *dArray;
+    dArray = new int[];
     return 0;
 }
